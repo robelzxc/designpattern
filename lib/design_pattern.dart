8189cbe2772ab794e41c3a12d8479d379c7a499c
robelzxc <robeljohnsongcog9@gmail.com>
2023-03-18 11:10:09 +0800

    Singleton Pattern

--- lib/design_pattern.dart
+++ lib/design_pattern.dart
@@ -1,3 +1,23 @@
-int calculate() {
-  return 6 * 7;
+void main(){
+  SingletonData variableA = new SingletonData();
+  SingletonData variableB = new SingletonData();
+  print(variableA._test);
+  variableA.setTest('Hello This is Variable Check!');
+  print(variableA._test);
+  print(variableB._test);
+}
+
+
+class SingletonData{
+  SingletonData.internal();
+  static final _singleton = SingletonData.internal();
+
+  String? _test;
+  String? get test=>_test;
+  void setTest(var newvalue){
+    _test = newvalue;
+  }
+  factory SingletonData(){
+    return _singleton;
+  }
 }
